208854e4730b7ceefe7146b35b9c436511a35d48
haroune <thessalonikaathena@outlook.com>
2025-03-07 15:46:09 +0100

    Add entrypoint for the project

---
+++ src/ai_backend/__init__.py
@@ -0,0 +1,10 @@
+# pylint: disable=C0114
+
+def main():
+    """
+    Entry point for ai_backend.
+    :return:
+    """
+    print("Welcome to LinShare AI Backend")
+if __name__ == "__main__":
+    main()

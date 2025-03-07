d4f93d2a26b74d0c39bec575173d594c8833f93c
haroune <thessalonikaathena@outlook.com>
2025-03-07 15:46:44 +0100

    Add tests folder

---
+++ tests/test_ai_backend.py
@@ -0,0 +1,17 @@
+# pylint: disable=C0114
+# pylint: disable= C0116
+
+import unittest
+
+class AIBackendTest(unittest.TestCase):
+    """
+    Unit tests for the ai backend.
+    """
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_init_db(self):
+        pass
